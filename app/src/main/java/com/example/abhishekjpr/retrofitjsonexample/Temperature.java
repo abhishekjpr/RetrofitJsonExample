57b585ff0c4b0a3f3a3ba4faf5d41034d5cd0aeb
abhishekjpr <abhishekjpr@outlook.com>
2016-06-19 10:19:00 +0530

    LastUpdated

---
+++ app/src/main/java/com/example/abhishekjpr/retrofitjsonexample/Temperature.java
@@ -0,0 +1,48 @@
+package com.example.abhishekjpr.retrofitjsonexample;
+
+/**
+ * Created by abhishekjpr on 18/6/16.
+ */
+public class Temperature {
+    String maxTemp, minTemp, name, status, description;
+
+    public String getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(String maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public String getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(String minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/example/abhishekjpr/retrofitjsonexample/WeatherAPI.java
@@ -0,0 +1,7 @@
+package com.example.abhishekjpr.retrofitjsonexample;
+
+/**
+ * Created by abhishekjpr on 18/6/16.
+ */
+public class WeatherAPI {
+}
